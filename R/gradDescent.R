082a123d168cd445d07d9de1667cb37dd0c7623a
Conor Newton <conornewton@gmail.com>
2020-12-27 21:54:14 +0000

    added gradient descent function

---
+++ R/gradDescent.R
@@ -0,0 +1,8 @@
+
+#' Finds the location of a local minimum of a function using gradient descent.
+#' 
+#' @param f A single valued function.
+#' @return A local minimum of the function f.
+gradDescent <- function(f) {
+
+}
