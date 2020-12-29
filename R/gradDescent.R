ed840b1b8d9d805fddb6666c65b2b57567450160
Conor Newton <conornewton@gmail.com>
2020-12-29 18:48:15 +0000

    gradDescent is fully tested and working

--- R/gradDescent.R
+++ R/gradDescent.R
@@ -1,6 +1,6 @@
 # Author: Conor Newton (conornewton@gmail.com)
 
-# TODO: refactor code
+# TODO: Fix norm calculations
 
 #' Finds the location of a local minimum of a function using gradient descent.
 #'
@@ -12,8 +12,7 @@
 #' @param step_method The function to be used to calculate the step size on each iteration (default BLS)
 #' @return A local minimum of the function f.
 #' @export
-grad_descent <- function(f, x, grad_f, n = 999, tol = 1e-7, step_method = "BB") {
-
+grad_descent <- function(f, x, grad_f, n = 999, tol = 1e-7, step_method = "BLS") {
     if (missing(grad_f)) {
         # Finds the gradient numerically using finite differencing
         delta <- 0.00001
@@ -29,44 +28,62 @@ grad_descent <- function(f, x, grad_f, n = 999, tol = 1e-7, step_method = "BB") 
 
     if (step_method == "BLS") {
         # Backtracking line search to calculate step size
-        step_size <- function() {
-            beta <- 0.5
-            step <- 1
-            t <-  0.5 * sum(grad_f(x) * grad_f(x))
-            while (f(x) - f(x - step * grad_f(x)) < step * t) {
-                step <- beta * step
-            }
-            return(step)
-        }
+        return(bls(f, x, grad_f, n, tol))
     }
     else if (step_method == "BB") {
         # Barzilai-Borwein Method to detemine new step size
-        step_size <- function() {
-            # If first iteration we do not have an old_x value to use
-            if (i == 1) {
-                old_x <- x
-                print("hello")
-                return(0.00001)
-            }
-            grad_diff <- (grad_f(x) - grad_f(old_x))
-            print(grad_diff)
-            step <- sum((x - old_x) * grad_diff) / (sum(grad_diff * grad_diff))
-            old_x <- x
-            print(step)
-            return(step)
-        }
+        return(bb(f, x, grad_f, n, tol))
+    }
+    else if (is.numeric(step_method) && length(step_method) == 1) {
+        # Constant step-size
+        return(con(f, x, grad_f, n, tol, step_method))
     }
     else {
-        # Constant step-size
-        step_size <- function() 0.00001
+        # Default constant step-size
+        warning("step_method argument invalid, defaulted to a constant step_size of 0.00001")
+        return(con(f, x, grad_f, n, tol, 0.00001))
     }
 
-    old_x <- x
+}
 
+bls <- function(f, x, grad_f, n, tol) {
     for (i in 1:n) {
         # Stop if the gradient is sufficiently small
         if (sum(grad_f(x) * grad_f(x)) < tol) return(x)
-        x <- x - step_size() * grad_f(x)
+
+        beta <- 0.5
+        step_size <- 1
+        t <-  0.5 * sum(grad_f(x) * grad_f(x))
+        while (f(x) - f(x - step_size * grad_f(x)) < step_size * t) {
+            step_size <- beta * step_size
+        }
+
+        x <- x - step_size * grad_f(x)
+    }
+    return(x)
+}
+
+bb <- function(f, x, grad_f, n, tol) {
+    step_size <- 0.00001
+    for (i in 1:n) {
+        # Stop if the gradient is sufficiently small
+        if (sum(grad_f(x) * grad_f(x)) < tol) return(x)
+
+        old_x <- x
+        x <- x - step_size * grad_f(x)
+
+        grad_diff <- (grad_f(x) - grad_f(old_x))
+        step_size <- sum((x - old_x) * grad_diff) / (sum(grad_diff * grad_diff))
+    }
+    return(x)
+
+}
+
+con <- function(f, x, grad_f, n, tol, step_size) {
+    for (i in 1:n) {
+        # Stop if the gradient is sufficiently small
+        if (sum(grad_f(x) * grad_f(x)) < tol) return(x)
+        x <- x - step_size * grad_f(x)
     }
     return(x)
 }
--- tests/testthat/testGradDescent.R
+++ tests/testthat/testGradDescent.R
@@ -12,3 +12,27 @@ test_that("grad_descent using numerical gradient", {
                  c(-1.5, -1),
                  tolerance = 1e-4)
 })
+
+test_that("different step method", {
+    f <- function(x) x[1]^2 + x[2]^2 + 3 * x[1] + 2 * x[2] + 9
+    expect_equal(grad_descent(f, c(0, 0), step_method = "BB"),
+                 c(-1.5, -1),
+                 tolerance = 1e-4)
+    expect_equal(grad_descent(f, c(0, 0), step_method = "BLS"),
+                 c(-1.5, -1),
+                 tolerance = 1e-4)
+    expect_equal(grad_descent(f, c(0, 0), step_method = 0.01),
+                 c(-1.5, -1),
+                 tolerance = 1e-4)
+})
+
+test_that("rosebrock function", {
+    f <- function(x) (1 - x[1])^2 + 100 * (x[2] - x[1]^2)^2
+
+    expect_equal(grad_descent(f, c(0, 0), n = 100000, step_method = "BB"),
+                 c(1, 1),
+                 tolerance = 1e-2)
+    expect_equal(grad_descent(f, c(0, 0), n = 100000, step_method = "BLS"),
+                 c(1, 1),
+                 tolerance = 1e-2)
+})
